d6eb54a0493c801923d033c9b11b2251aaa6431c
Cindy Nguyen <cnguyen5138@gmail.com>
2024-06-14 11:21:32 -0400

    did the from scratch portion

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,2 +1,19 @@
-export const fetchHandler = () => {
+export const fetchHandler = async (url, option = {}) => {
+    try {
+        const response = await fetch(url, option)
+        if (!response.ok) {
+            throw new Error(`Fetch failed with status - ${response.status}, ${response.statusText}`)
+        }
+        const isJson = (response.headers.get('content-type') || '').includes('application/json');
+        if (isJson) {
+            const jsonData = await response.json()
+            return [jsonData, null]
+        }
+        const textData = await response.text()
+        return [textData, null]
+    }
+    catch (error) {
+        console.warn(error)
+        return [null, error]
+    }
 };
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
-import {
-  fetchHandler
-} from './from-scratch';
+// import {
+//   fetchHandler
+// } from './from-scratch';
 
-const appDiv = document.querySelector('#app');
+// const appDiv = document.querySelector('#app');
+
+const getUser = async (userId) => {
+  const url = `https://jsonplaceholder.typicode.com/users/${userId}`;
+  const response = await fetch(url);
+  const users = await response.json();
+  console.log(users)
+  return users;
+}
+getUser(-1)
